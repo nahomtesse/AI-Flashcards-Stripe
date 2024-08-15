a546d0f4e3e5d2246523208c22c4c62cd1c6ac6d
Nahom <gedanahom@gmail.com>
2024-08-15 10:45:43 -0700

    Improved design for sign-in/sign-up pages

--- app/sign-in/[[...sign-in]]/page.js
+++ app/sign-in/[[...sign-in]]/page.js
@@ -2,38 +2,33 @@ import { AppBar, Container, Toolbar, Link, Button, Typography, Box } from "@mui/
 import { SignIn } from '@clerk/nextjs'
 
 
-export default function SignUpPage()
-{
+export default function SignInPage() {
     return (
-        <Container maxWidth='sm'>
-            <AppBar position="static" sx={{backgroundColor: "#081C15"}}>
-                <Toolbar > 
-                    <Typography variant="h6" sx={{
-                        flexGrow: 1
-                    }}>
-                        Flashcard Saas
-                    </Typography>
-                </Toolbar>
-
-                <Button color='inherit' >
-                    <Link href="/">Home</Link>
-                </Button>
-
-                <Button color='inherit'>
-                    <Link href="/sign-in" password>Login</Link>
-                </Button>
-
-                <Button color='inherit'>
-                    <Link href="/sign-up" password>Signup</Link>
-                </Button>
-            </AppBar>
-
-            <Box display='flex' flexDirection="column" alignItems='center' justifyContent='center'>
-                    <Typography variant='h4'>
+        <Box sx={{ backgroundColor: "#081C15", minHeight: "100vh", paddingTop: 4 }}>
+            <Container maxWidth='sm'>
+                <AppBar position="static" sx={{ backgroundColor: "#142C21" }}>
+                    <Toolbar>
+                        <Typography variant="h6" sx={{ flexGrow: 1, color: "#D8F3DC" }}>
+                            Flashcard Saas
+                        </Typography>
+                        <Button color='inherit'>
+                            <Link href="/" sx={{ color: "#D8F3DC"}}>Home</Link>
+                        </Button>
+                        <Button color='inherit'>
+                            <Link href="/sign-in" sx={{ color: "#D8F3DC"}}>Login</Link>
+                        </Button>
+                        <Button color='inherit'>
+                            <Link href="/sign-up" sx={{ color: "#D8F3DC"}}>Signup</Link>
+                        </Button>
+                    </Toolbar>
+                </AppBar>
+                <Box display='flex' flexDirection="column" alignItems='center' justifyContent='center' sx={{ color: "#000000", mt: 4, p: 2, borderRadius: 1, backgroundColor: '#FFF', boxShadow: 3 }}>
+                    <Typography variant='h4' sx={{ marginBottom: 2 }}>
                         Signin
                     </Typography>
                     <SignIn />
-            </Box>
-        </Container>
+                </Box>
+            </Container>
+        </Box>
     )
 }
